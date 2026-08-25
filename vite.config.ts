7cdf72dfb0b6b167fadc0cde8b44eb6d53c6326f
Luis Enrique Ojeda <luisenriqueo86@gmail.com>
2026-08-25 10:10:50 -0300

    Configura Luque Spine para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/luque-spine/',
    plugins: [
   react(),
   legacy(),
@@ -20,15 +21,16 @@ export default defineConfig({
       theme_color: '#ffffff',
       background_color: '#ffffff',
       display: 'standalone',
-      start_url: '/',
+      start_url: '/luque-spine/',
+scope: '/luque-spine/',
       icons: [
         {
-          src: '/icon-192.png',
+           src: 'icon-192.png',
           sizes: '192x192',
           type: 'image/png',
         },
         {
-          src: '/icon-512.png',
+          src: 'icon-512.png',
           sizes: '512x512',
           type: 'image/png',
         },
