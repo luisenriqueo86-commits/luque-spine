1270745d4ac6a526aa4309c070e82eb6c7b6aaad
Luis Enrique Ojeda <luisenriqueo86@gmail.com>
2026-09-14 21:34:38 -0300

    Updates

--- assets/p-DIE4pXMl-legacy-c2fpSpv6.js
+++ assets/p-DIE4pXMl-legacy-c2fpSpv6.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-KCiz4SPf.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.B,i=e.r}],execute:function(){
+System.register(["./index-legacy-CxFO3T2e.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.B,i=e.r}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
--- assets/p-Sh0ICmPV-legacy-Dv2D9IRB.js
+++ assets/p-Sh0ICmPV-legacy-Dv2D9IRB.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-KCiz4SPf.js"],function(t,e){"use strict";var n,r,s;return{setters:[t=>{n=t.o,r=t.s,s=t.b}],execute:function(){
+System.register(["./index-legacy-CxFO3T2e.js"],function(t,e){"use strict";var n,r,s;return{setters:[t=>{n=t.o,r=t.s,s=t.b}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
