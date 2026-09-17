91edefd0345d7589d3f45b62f64c55546104f702
Luis Enrique Ojeda <luisenriqueo86@gmail.com>
2026-09-17 18:17:40 -0300

    Updates

--- assets/p-D7bSXQD3-legacy-BmLHlYNC.js
+++ assets/p-D7bSXQD3-legacy-BmLHlYNC.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-oi0YGfRx.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.H,r=e.m,s=e.e,o=e.P,i=e.c}],execute:function(){
+System.register(["./index-legacy-q4CXu0MS.js"],function(e,t){"use strict";var n,r,s,o,i;return{setters:[e=>{n=e.H,r=e.m,s=e.e,o=e.P,i=e.c}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
--- assets/p-DIE4pXMl-legacy-C9rxWnNW.js
+++ assets/p-DIE4pXMl-legacy-C9rxWnNW.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-oi0YGfRx.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.B,i=e.r}],execute:function(){
+System.register(["./index-legacy-q4CXu0MS.js"],function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.B,i=e.r}],execute:function(){
 /*!
 			 * (C) Ionic http://ionicframework.com - MIT License
 			 */
